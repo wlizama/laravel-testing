aa012d19585f977ca96d682efd1a40762866f9bc
Wilder Lizama <wilderlizama@gmail.com>
2018-01-05 00:33:19 -0500

    Validacion Formulario galeria/create

--- resources/views/galeria/image-form.blade.php
+++ resources/views/galeria/image-form.blade.php
@@ -5,22 +5,34 @@
     <div class="col-12">
       <form action="/galeria/create" method="post">
         {{ csrf_field() }}
-        <div class="form-group">
-          <label for="txtTitle">Título de la imagen</label>
-          <input id="txtTitle" name="title" type="text" class="form-control">
+        <div class="row">
+          <div class="col-12 form-group @if($errors->any()) has-danger @endif">
+            <label for="txtTitle">Título de la imagen</label>
+            <input id="txtTitle" name="title" type="text" class="form-control @if($errors->has('title')) form-control-danger @endif">
+          </div>
+
+          <div class="col-12">
+            <label for="txtDescription">Descripción</label>
+            <input id="txtDescription" name="descripcion" type="text" class="form-control">
+          </div>
+
+          {{-- <div class="form-group">
+            <label for="txtImageURL">Image URL</label>
+            <input id="txtImageURL" name="url" type="url" class="form-control">
+          </div> --}}
+          
+          <div class="@if($errors->any()) has-danger @endif"">
+            @if($errors->any())
+               @foreach ($errors->all() as $error)
+                  <div class="col-12 form-control-feedback">{{ $error }}</div>
+              @endforeach
+            @endif
+          </div>
+          
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary">Guardar</button>
+          </div>
         </div>
-
-        <div class="form-group">
-          <label for="txtDescription">Descripción</label>
-          <input id="txtDescription" name="descripcion" type="text" class="form-control">
-        </div>
-
-        {{-- <div class="form-group">
-          <label for="txtImageURL">Image URL</label>
-          <input id="txtImageURL" name="url" type="url" class="form-control">
-        </div> --}}
-
-        <button type="submit" class="btn btn-primary">Guardar</button>
       </form>
     </div>
   </div>
